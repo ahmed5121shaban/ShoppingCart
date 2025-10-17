7002ea660c94957e1e774e22ff1c04966c04ea5c
AhmedShaban <ahmed.sh5121999@gmail.com>
2025-10-17 22:31:41 +0300

    Refactor to use controllers and add ShoppingCartController

---
+++ ShoppingCart/Controllers/ShoppingCart/ShoppingCartController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingCart.Controllers.ShoppingCart
+{
+    [Route("/")] // instead of ( api/[controller] )
+    [ApiController]
+    public class ShoppingCartController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult HealthCheck()
+        {
+            return Ok("Hello World!");
+        }
+    }
+}
